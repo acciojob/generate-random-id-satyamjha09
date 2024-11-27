ea6809eba2aa9808e00469681ee14265d52e8103
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-27 20:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
-  // write your code here
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = ''
+	for(let i = 0; i < l; i++) {
+		const randomIndex = Math.floor(Math.random() * characters);
+		result += characters.charAt(randomIndex)
+	}
+	return result
 }
 
 // Do not change the code below.
